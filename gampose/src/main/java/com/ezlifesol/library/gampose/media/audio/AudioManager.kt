3395f123ba7dbce1ec628b242559e3595fc14001
AnhHQ2 <AnhHQ2@lpbank.com.vn>
2024-08-27 08:31:30 +0700

    Add 3D Audio Support to AudioManager
    - Enhanced `AudioManager` with 3D sound capabilities using SoundPool.
    - Updated `registerSounds` and `playSound` methods to handle 3D audio effects based on the `GameState`.
    - Added volume adjustment for left and right audio channels based on the position of the sound source relative to the screen center.
    - Introduced new logic to calculate volume and balance for sound effects to create a more immersive audio experience.
    - Refactored `playSound` method to use `gameState` for 3D audio positioning and volume adjustment.

--- gampose/src/main/java/com/ezlifesol/library/gampose/media/audio/AudioManager.kt
+++ gampose/src/main/java/com/ezlifesol/library/gampose/media/audio/AudioManager.kt
@@ -31,10 +31,14 @@ import android.media.MediaPlayer
 import android.media.SoundPool
 import androidx.annotation.Keep
 import androidx.annotation.RawRes
+import com.ezlifesol.library.gampose.compose.GameState
 import com.ezlifesol.library.gampose.log.debugLog
+import com.ezlifesol.library.gampose.unit.GameVector
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlin.math.atan2
+import kotlin.math.hypot
 
 /**
  * AudioManager is a singleton object responsible for managing audio playback in the game.
@@ -46,6 +50,8 @@ object AudioManager {
     private var mediaPlayer: MediaPlayer? = null
     private val sounds = mutableMapOf<Int, Int>()
     private var soundPool: SoundPool? = null
+
+    private var gameState: GameState? = null
 
     /**
      * Plays background music from a raw resource.
@@ -97,13 +103,18 @@ object AudioManager {
         }
     }
 
+
     /**
      * Registers sound effects from raw resources with SoundPool.
      *
-     * @param context The context to access resources.
-     * @param resIds Vararg parameter of resource IDs for sound effects to be registered.
+     * @param context The context used to access resources.
+     * @param gameState Optional: A GameState object containing the game size and game vision. If provided, it will be used to set the screen dimensions and position for 3D sound effects.
+     * @param resIds Vararg parameter of resource IDs for the sound effects to be registered.
      */
-    fun registerSounds(context: Context, @RawRes vararg resIds: Int) {
+    fun registerSounds(context: Context, gameState: GameState? = null, @RawRes vararg resIds: Int) {
+        gameState?.let {
+            this.gameState = gameState
+        }
         CoroutineScope(Dispatchers.IO).launch {
             soundPool ?: run {
                 val audioAttributes = AudioAttributes.Builder()
@@ -126,20 +137,64 @@ object AudioManager {
     }
 
     /**
+     * Registers sound effects from raw resources with SoundPool.
+     * This overloaded function allows you to register sounds without specifying a game state.
+     *
+     * @param context The context used to access resources.
+     * @param resIds Vararg parameter of resource IDs for the sound effects to be registered.
+     */
+    fun registerSounds(context: Context, @RawRes vararg resIds: Int) {
+        registerSounds(context = context, gameState = gameState, resIds = resIds)
+    }
+
+
+    /**
      * Plays a registered sound effect.
      *
      * @param resId The resource ID of the sound effect to play.
+     * @param source Optional: The position of the sound source in the game. If not provided, the sound will be played with equal volume on both channels.
      */
-    fun playSound(@RawRes resId: Int) {
+    fun playSound(@RawRes resId: Int, source: GameVector? = null) {
         CoroutineScope(Dispatchers.IO).launch {
             val soundId = sounds[resId]
+
             soundPool?.let { soundPool ->
                 soundId?.let {
-                    soundPool.play(it, 1f, 1f, 1, 0, 1f)
+                    val (leftVolume, rightVolume) = source?.let { src ->
+                        gameState?.let { state ->
+                            val screenCenter = state.gameVision.position
+                            val screenWidth = state.gameSize.width
+
+                            // Calculate the distance and angle between the screen center and the source
+                            val deltaX = src.x - screenCenter.x
+                            val deltaY = src.y - screenCenter.y
+
+                            // Calculate the angle from the screen center to the sound source
+                            val angle = atan2(deltaY, deltaX)
+
+                            // Adjust volume based on angle and distance
+                            val distance = hypot(deltaX.toDouble(), deltaY.toDouble()).toFloat()
+
+                            // Set maxDistance to the screen width times 1.5
+                            val maxDistance = screenWidth * 1.5f
+
+                            // Calculate the volume, assuming it decreases as the distance increases
+                            val volume = (1 - distance / maxDistance).coerceIn(0f, 1f)
+
+                            // Calculate the volume for the left and right channels based on the angle
+                            val leftVol = volume * (1 - Math.sin(angle.toDouble()).toFloat()) / 2
+                            val rightVol = volume * (1 + Math.sin(angle.toDouble()).toFloat()) / 2
+
+                            leftVol to rightVol
+                        } ?: (1f to 1f) // If gameState is not provided, play the sound with equal volume on both channels
+                    } ?: (1f to 1f) // If no source is provided, play the sound with equal volume on both channels
+
+                    soundPool.play(it, leftVolume, rightVolume, 1, 0, 1f)
                 } ?: debugLog("Sound not found: $resId")
             }
         }
     }
+
 
     /**
      * Releases all resources used by MediaPlayer and SoundPool.
